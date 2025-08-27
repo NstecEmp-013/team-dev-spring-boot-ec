b963e3d9f3c6fe0e9b7a33225c6646afc099f00f
NstecEmp-011 <nstecemp_011@fullness.email>
2025-08-27 10:30:19 +0900

    UC009 yunoki

---
+++ src/main/java/com/fullness/ec/entity/AccountCategories.java
@@ -0,0 +1,13 @@
+package com.fullness.ec.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AccountCategories {
+    private String accountName;
+    private String password;
+    private Integer accountId;
+}
